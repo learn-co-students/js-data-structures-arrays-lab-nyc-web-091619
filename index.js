46bf19d36aa145119dc7529dd79b5d4381185fb1
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-10-27 16:39:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Write your solution here!
+// Write your solution here
+
+// drivers
+// ✓ is assigned an initial value of ["Milo", "Otis", "Garfield"]
+const drivers = ['Milo', 'Otis', 'Garfield']
+
+
+// destructivelyAppendDriver(name)
+// ✓ appends a driver to the end of the drivers array
+function destructivelyAppendDriver(name){
+    drivers.push(name);
+}
+
+
+// destructivelyPrependDriver(name)
+// ✓ prepends a driver to the beginning of the drivers array
+function destructivelyPrependDriver(name){
+    drivers.unshift(name);
+}
+
+// destructivelyRemoveLastDriver()
+// ✓ removes the last driver from the drivers array
+function destructivelyRemoveLastDriver(){
+    drivers.pop();
+}
+
+
+// destructivelyRemoveFirstDriver()
+// ✓ removes the First driver from the drivers array
+function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+}
+
+
+// appendDriver(name)
+// ✓ appends a driver to the drivers array and returns a new array, leaving the drivers array unchanged
+function appendDriver(name){
+    return [...drivers.slice(0), name];
+}
+
+// prependDriver(name)
+// ✓ prepends a driver to the drivers array and returns a new array, leaving the drivers array unchanged
+function prependDriver(name){
+    return [name,...drivers.slice(0)];
+}
+
+
+// removeLastDriver()
+// ✓ removes the last driver in the drivers array and returns a new array, leaving the drivers array unchanged
+function removeLastDriver(){
+    return drivers.slice(0,2);
+}
+
+
+// removeFirstDriver()
+// ✓ removes the first driver from the drivers array and returns a new array, leaving the drivers array unchanged
+function removeFirstDriver(){
+    return drivers.slice(1,3);
+}
